ed9d11d0dd2f78dd540e3d13abd8be5efe804711
Ivan Kwiatkowski <justicerage@manalyzer.org>
2024-04-11 21:23:09 +0200

    Use the latest gpt-4-turbo model.

--- gepetto/models/base.py
+++ gepetto/models/base.py
@@ -1,7 +1,7 @@
 import abc
 
 GPT3_MODEL_NAME = "gpt-3.5-turbo-1106"
-GPT4_MODEL_NAME = "gpt-4-1106-preview"
+GPT4_MODEL_NAME = "gpt-4-turbo"
 
 class LanguageModel(abc.ABC):
     @abc.abstractmethod
@@ -13,7 +13,6 @@ def get_model(model):
     """
     Instantiates a model based on its name
     :param model: The model to use
-    :param config: The object containing the configuration of the program
     :return:
     """
     if model == GPT3_MODEL_NAME or model == GPT4_MODEL_NAME:
