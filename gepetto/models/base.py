59dbf471eaac9cea3df612c2dcb77781234249dc
Ivan Kwiatkowski <justicerage@manalyzer.org>
2024-08-31 13:21:33 +0200

    Updated the default model used by Groq (fixes #42).

--- gepetto/models/base.py
+++ gepetto/models/base.py
@@ -3,7 +3,7 @@ import abc
 GPT3_MODEL_NAME = "gpt-3.5-turbo-0125"
 GPT4_MODEL_NAME = "gpt-4-turbo"
 GPT4o_MODEL_NAME = "gpt-4o"
-GROQ_MODEL_NAME = "llama3-70b-8192"
+GROQ_MODEL_NAME = "llama-3.1-70b-versatile"
 MISTRAL_MODEL_NAME = "mistralai/Mixtral-8x22B-Instruct-v0.1"
 
 
