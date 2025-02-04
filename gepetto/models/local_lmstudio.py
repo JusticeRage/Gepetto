429c1c3b51c7f7c96d3de38e8ea0436893750388
Raúl Álvarez <raulwii2@gmail.com>
2025-02-04 18:36:53 +0100

    Cache models and set timeout

--- gepetto/models/local_lmstudio.py
+++ gepetto/models/local_lmstudio.py
@@ -5,6 +5,8 @@ import openai
 import gepetto.models.model_manager
 import gepetto.config
 
+LMSTUDIO_MODELS = None
+
 class LMStudio(GPT):
     @staticmethod
     def get_menu_name() -> str:
@@ -12,14 +14,26 @@ class LMStudio(GPT):
 
     @staticmethod
     def supported_models() -> list:
+        global LMSTUDIO_MODELS
+
+        if LMSTUDIO_MODELS is not None:
+            return LMSTUDIO_MODELS
+
         base_url = gepetto.config.get_config("LMStudio", "BASE_URL", "http://127.0.0.1:1234/v1/")
-        response = _httpx.get(f"{base_url}models")
-        if response.status_code == 200:
-            models = response.json().get("data", [])
-            return [model["id"] for model in models]
-        else:
-            print(_("Failed to fetch models from {base_url}: {status_code}").format(base_url=base_url, status_code=response.status_code))
-            return []
+        try:
+            response = _httpx.get(f"{base_url}models", timeout=2)
+            if response.status_code == 200:
+                data = response.json().get("data", [])
+                LMSTUDIO_MODELS = [model["id"] for model in data]
+            else:
+                print(_("Failed to fetch models from {base_url}: {status_code}").format(
+                    base_url=base_url, status_code=response.status_code
+                ))
+                LMSTUDIO_MODELS = []
+        except (_httpx.ConnectError, _httpx.ConnectTimeout):
+            LMSTUDIO_MODELS = []
+
+        return LMSTUDIO_MODELS
 
     @staticmethod
     def is_configured_properly() -> bool:
@@ -61,8 +75,5 @@ class LMStudio(GPT):
         super().query_model(query, cb, additional_model_options)
 
     # -----------------------------------------------------------------------------
-
-    def query_model_async(self, query, cb, additional_model_options=None):
-        super().query_model_async(query, cb, additional_model_options)
 
 gepetto.models.model_manager.register_model(LMStudio)
